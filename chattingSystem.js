aef1a7b08250a6fa36520fc07b13e9b303b3e8d0
Jess Bond <JBond45@users.noreply.github.com>
2025-11-01 21:00:27 +0000

    fixed server for GET

--- chattingSystem.js
+++ chattingSystem.js
@@ -1,42 +1,40 @@
+//set up talk session
+
 //set up server
 const express = require('express');
 const app = express();
 const port = 8080;
 
 
-//set up talk session
-import { getTalkSession } from 'https://cdn.jsdelivr.net/npm/@talkjs/core@1.5.8';
-
-const appId = 'tJU4ulfD';
-
-const userId = 'Server';
-const otherUserId = 'User';
-const conversationId = 'globalConv';
-
-const session = getTalkSession({
-    // @ts-ignore
-    host: 'durhack.talkjs.com',
-    appId,
-    userId,
-});
-
-session.currentUser.createIfNotExists({ name: 'Server' });
-session.user(otherUserId).createIfNotExists({ name: 'Me' });
-
-const conversation = session.conversation(conversationId);
-conversation.createIfNotExists();
-conversation.participant(otherUserId).createIfNotExists();
 //listen for input
 
 
 // Define a route for GET requests to the root URL
 app.get('/send', (req, res) => {
   let { username, message } = req.query;
-  conversation.send(username+": "+message);
+  console.log(username +":"+ message);
+    return fetch(
+    `https://api-durhack.talkjs.com/v1/tJU4ulfD/conversations/globalConv/messages`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer sk_test_NFYeWRuqB7dgHwEv382IbOJMdQUjj1RE`,
+      },
+      body: JSON.stringify([
+        {
+          text: username+": "+message,
+          sender: "Server",
+          type: "UserMessage",
+        },
+      ]),
+    }
+  );
 });
 
 // Start the server
 app.listen(port, () => {
+
   console.log(`Example app listening at http://localhost:${port}`);
 }); 
 
