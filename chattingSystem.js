bb044bd14941c60db320f00e68251ad67745d89d
Jess Bond <JBond45@users.noreply.github.com>
2025-11-01 18:51:27 +0000

    simplify and settup express js server

--- chattingSystem.js
+++ chattingSystem.js
@@ -1,34 +1,43 @@
+//set up server
+const express = require('express');
+const app = express();
+const port = 8080;
+
+
+//set up talk session
 import { getTalkSession } from 'https://cdn.jsdelivr.net/npm/@talkjs/core@1.5.8';
+
 const appId = 'tJU4ulfD';
 
-const userId = null;
-const otherUserId = 'Nina';
-const conversationId = 'my_conversation2';
+const userId = 'Server';
+const otherUserId = 'User';
+const conversationId = 'globalConv';
 
 const session = getTalkSession({
     // @ts-ignore
     host: 'durhack.talkjs.com',
     appId,
-    userId,});
+    userId,
+});
 
-session.currentUser.createIfNotExists({ name: 'Frank' });
-session.user(otherUserId).createIfNotExists({ name: 'Nina' });
+session.currentUser.createIfNotExists({ name: 'Server' });
+session.user(otherUserId).createIfNotExists({ name: 'Me' });
 
 const conversation = session.conversation(conversationId);
 conversation.createIfNotExists();
 conversation.participant(otherUserId).createIfNotExists();
+//listen for input
 
 
-function login(){
-    username = document.getElementById("userName").value;
-    console.log(username);
-    document.getElementById("userForm").setAttribute("hidden","hidden");
-    document.getElementById("chatDiv").removeAttribute("hidden");
-    document.getElementById("chatBox").setAttribute("user-id","Frank"); // change to username in future
+// Define a route for GET requests to the root URL
+app.get('/send', (req, res) => {
+  let { username, message } = req.query;
+  conversation.send(username+": "+message);
+});
 
-}
-
-
-document.getElementById("submitUser").addEventListener('click',login);
+// Start the server
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+}); 
 
 
